009f69fa714a307fd2d26ed3bc0cc4ffc97f9577
indiangravy <60591283+indiangravy@users.noreply.github.com>
2023-02-16 19:43:14 -0500

    Spring Boot basic upload

---
+++ API/src/main/java/com/example/charitylink/CharityLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.charitylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityLinkApplication.class, args);
+    }
+
+}
