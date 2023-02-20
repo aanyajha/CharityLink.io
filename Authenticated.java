3ac8e3c57f7dd4d5a762472f494e05353fcc37bf
Aanya Jha <90077877+aanyajha@users.noreply.github.com>
2023-02-19 21:58:16 -0500

    Created Authenticated.java
    Marks methods that need authentication

---
+++ Authenticated.java
@@ -0,0 +1,8 @@
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authenticated {
+}
